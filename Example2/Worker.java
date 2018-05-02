72b2b788907edd26ba6d2227214fc1bd7bfca682
Rafael Mena <rmena@nearsoft.com>
2018-05-02 13:03:09 -0700

    first commit

---
+++ Example2/Worker.java
@@ -0,0 +1,47 @@
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+
+public class Worker {
+
+    private final static String TASK_QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws java.io.IOException, java.lang.InterruptedException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, false,false,false,null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(" [x] Done");
+                }
+            }
+        };
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, consumer);
+
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
+    }
+
+}
